9ec623047e35c97b3840c588b75adedae64aa60d
unknown <saiful.bahri2004@gmail.com>
2026-03-02 12:29:16 +0700

    update pwa

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withPWA from 'next-pwa';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {}, // ← ini penting jangan dihapus karena Next.js 14 sudah pakai Turbopack secara default, tapi masih dalam tahap eksperimental. Dengan menambahkan ini, kita bisa memastikan Turbopack tetap aktif walaupun ada fitur lain yang mungkin belum kompatibel.
 };
 
 export default withPWA({
